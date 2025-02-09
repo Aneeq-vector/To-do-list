69f8ded18e5d6496a9d7f2eb27d4734b3cf8d434
Ahmed Aneeq <ahmedaneeq.official@gmail.com>
2025-02-09 20:48:25 +0530

    Edited
    09/02/2025

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 //selecting ul
 var ul = document.getElementById("list-container")
 
+var input = document.getElementById("input")
+
+window.onload = function() {
+    ul.innerHTML = ""; // Removes all list items
+}
+
 function add()
 {
     var listitem = document.createElement("li")
-    listitem.textContent = "Hello"
+    listitem.innerHTML = input.value + "<button onclick='deleteItem(event)'>Delete</button>"
     ul.append(listitem)
+}
+
+function deleteItem(event)
+{
+    event.target.parentElement.remove("li")
 }
